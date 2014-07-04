dc85e98e38c7b77f23b9a0312ee7de9b53efb33b
yoursfengzhilian <yoursfengzhilian@664461a0-645f-4455-b1c3-4701dfd24dbe>
2014-07-04 08:44:01 +0000

    添加工程，建立目录结构

---
+++ trunk/src/TestBinTree/bSearchTree.hpp
@@ -0,0 +1,153 @@
+#ifndef ___BSEARCHTREETYPE_H___
+#define ___BSEARCHTREETYPE_H___
+
+
+#include "binaryTree.hpp"
+
+template<class elemType>
+class bSearchTreeType : public binaryTreeType<elemType> {
+
+public:
+	bool search(const elemType& searchItem);
+
+	bool insert(const elemType& insertItem);
+
+	bool deleteNode(const elemType& deleteItem);
+
+private:
+	void deleteFromTree(nodeType<elemType>* &p);
+};
+
+template<class elemType>
+bool bSearchTreeType<elemType>::search(const elemType& searchItem){
+	nodeType<elemType> *current;
+	bool found = false;
+
+	if(root == NULL) {
+		cout << "Cannot search the empty tree." << endl;
+	} else {
+		current == root;
+
+		while(current != NULL && !found) {
+			if (current->info == searchItem)
+			{
+				found = true;
+			} else {
+				if (current->info > searchItem)
+				{
+					current = current->llink;
+				} else {
+					current = current->rlink
+				}
+			}
+		}
+	}
+
+	return found;
+}
+
+template<class elemType>
+void bSearchTreeType<elemType>::insert(const elemType& insertItem) {
+	nodeType<elemType> *current;
+	nodeType<elemType> *trailCurrent;
+	nodeType<elemType> *newNode;
+
+	newNode = new nodeType<elemType>;
+	newNode->info = insertItem;
+	newNode->llink = NULL;
+	newNode->rlink = NULL;
+
+	if (root == NULL)
+	{
+		root = newNode;
+	} else {
+		current = root;
+
+		while(current != NULL) 
+		{
+			trailCurrent = current;
+
+			if (current->info == insertItem)
+			{
+				cout << "the insert item already is in the list";
+				cout << "duplicates are not allowed." << insertItem << endl;
+				return;
+			} else {
+				if (current->info > insertItem)
+				{
+					current = current->llink;
+				} else {
+					current = current->rlink;
+				}
+			}
+		}
+
+		if (trailCurrent->info > insertItem)
+		{
+			trailCurrent->llink = newNode;
+		} else {
+			trailCurrent->rlink = newNode;
+		}
+	}
+}
+
+template<class elemType>
+void bSearchTreeType<elemType>::deleteFromTree(nodeType<elemType>* &p) {
+	nodeType<elemType> *current;
+	nodeType<elemType> *trailCurrent;
+	nodeType<elemType> *temp;
+
+	if (p == NULL)
+	{
+		cout << "Error: The node to be delete is NULL" << endl;
+	} else if (p->llink == NULL && p->rlink)
+	{
+		temp = p;
+		p = NULL;
+		delete temp;
+	} else if (p->llink == NULL)
+	{
+		temp = p;
+		p = temp->rlink;
+		delete temp;
+	} else if (p->rlink == NULL) {
+		temp = p;
+		p = temp->llink;
+		
+		delete temp;
+	} else {
+		current = p->llink;
+		trailCurrent = NULL;
+
+		while(current->rlink != NULL) {
+			trailCurrent = current;
+			current = current->rlink;
+		}
+
+		p->info = current->info;
+
+		if (trailCurrent == NULL)
+		{
+			p->llink = current->llink;
+		} else {
+			trailCurrent->rlink = current->llink;
+		}
+
+		delete current;
+	}// end else
+}
+
+template<class elemType>
+void bSearchTreeType<elemType>::deleteNode(const elemType& deleteItem) {
+	nodeType<elemType> *current;
+	nodeType<elemType> *trailCurrent;
+	bool found = false;
+
+	if(root == NULL) {
+		cout <<"Cannot delete from the empty tree." << endl;
+	} else {
+
+	}
+}
+
+#endif // end of ___BSEARCHTREETYPE_H___
---
+++ trunk/src/TestBinTree/binaryTree.hpp
@@ -0,0 +1,222 @@
+#ifndef ___BINARYTREE_H____ 
+#define ___BINARYTREE_H____
+
+// 二叉树节点定义
+template <class elemType>
+struct nodeType {
+	elemType info;
+	nodeType<elemType> *llink;
+	nodeType<elemType> *rlink;
+};
+
+template <class elemType>
+class binaryTreeType {
+public:
+	const binaryTreeType<type>& operator=(const binaryTreeType<type>&);
+
+	bool isEmpty();
+
+	void inorderTraversal();
+	void preorderTraversal();
+	void postorderTraversal();
+
+	int treeHeight();
+	int treeNodeCount();
+	int treeLeavesCount();
+
+	void destroyTree();
+
+	binaryTreeType(const binaryTreeType<elemType>& otherTree);
+	binaryTreeType();
+
+	~binaryTreeType();
+
+protected:
+	const nodeType<elemType> * const root;
+
+private:
+	void copyTree(nodeType<elemType>* &copiedTreeRoot,nodeType<elemType>* otherTreeRoot);
+
+	void destroy(nodeType<elemType>* &p);
+
+	void inorder(nodeType<elemType> *p);
+	void preorder(nodeType<elemType> *p);
+	void postorder(nodeType<elemType> *p);
+
+	int height(nodeType<elemType> *p);
+	int nodeCount(nodeType<elemType> *p);
+	int leavesCount(nodeType<elemType> *p);
+};
+
+template<class elemType>
+bool binaryTreeType<elemType>::isEmpty(){
+	return (root == NULL);
+}
+
+template<class elemType>
+binaryTreeType<elemType>::binaryTreeType(){
+	root = NULL;
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::inorderTraversal() {
+	inorder(root);
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::preorderTraversal() {
+	preorder(root);
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::postorderTraversal() {
+	postorder(root);
+}
+
+template<class elemType>
+int binaryTreeType<elemType>::treeHeight() {
+	return height(root);
+}
+
+template<class elemType>
+int binaryTreeType<elemType>::treeNodeCount() {
+	return nodeCount(root);
+}
+
+template<class elemType>
+int binaryTreeType<elemType>::treeLeavesCount() {
+	return leavesCount(root);
+}
+
+
+template<class elemType>
+void binaryTreeType<elemType>::inorder(nodeType<elemType> *p) {
+	if (p != NULL)
+	{
+		inorder(p->llink);
+		cout << p->info << " ";
+		inorder(p->rlink);
+	}
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::preorder(nodeType<elemType> *p) {
+	if (p != NULL)
+	{
+		cout << p->info << " ";
+		preorder(p->llink);
+		preorder(p->rlink);
+	}
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::postorder(nodeType<elemType> *p) {
+	if (p != NULL)
+	{
+		postorder(p->llink);
+		postorder(p->rlink);
+		cout << p->info << " ";
+	}
+}
+
+template<class elemType>
+int binaryTreeType<elemType>::height(nodeType<elemType> *p) {
+	if (p == NULL)
+	{
+		return 0;
+	} else {
+		return 1 + std::max(height(p->llink),height(p->rlink));
+	}
+}
+
+template<class elemType>
+int binaryTreeType<elemType>::nodeCount(nodeType<elemType> *p) {
+	int ic = 0;
+	if (p == NULL)
+	{
+		return ic;
+	} else {
+		// todo
+	}
+
+	return ic;
+}
+
+template<class elemType>
+int binaryTreeType<elemType>::leavesCount(nodeType<elemType> *p) {
+	int ic = 0;
+	if (p == NULL)
+	{
+		return ic;
+	} else {
+		// todo
+	} 
+
+	return ic;
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::copyTree(nodeType<elemType>* &copiedTreeRoot,nodeType<elemType>* otherTreeRoot) {
+	if (otherTreeRoot == NULL)
+	{
+		copiedTreeRoot = NULL;
+	} else {
+		copiedTreeRoot = new nodeType<elemType>;
+		copiedTreeRoot->info = otherTreeRoot->info;
+		copyTree(copiedTreeRoot->llink,otherTreeRoot->llink);
+		copyTree(copiedTreeRoot->rlink,otherTreeRoot->rlink);
+	}
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::destroy(nodeType<elemType>* &p) {
+	if (p != NULL)
+	{
+		destroy(p->llink);
+		destroy(p->rlink);
+		delete p;
+		p = NULL;
+	}
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::destroyTree() {
+	destroy(root);
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::binaryTreeType(const binaryTreeType<elemType>& otherTree) {
+	if (otherTree.root == NULL)
+	{
+		root = NULL;
+	} else {
+		copyTree(root,otherTree.root);
+	}
+}
+
+template<class elemType>
+void binaryTreeType<elemType>::~binaryTreeType() {
+	destroy(root);
+}
+
+template<class elemType>
+const binaryTreeType<type>& binaryTreeType<elemType>::operator=(const binaryTreeType<type>& otherTree) {
+	if (this != &otherTree)
+	{
+		if (root != NULL)
+		{
+			destroy(root);
+		}
+
+		if (otherTree.root == NULL)
+		{
+			root == NULL;
+		} else {
+			copyTree(root,otherTree.root);
+		}
+	}
+
+	return *this;
+}
+
+#endif // end of ___BINARYTREE_H____
